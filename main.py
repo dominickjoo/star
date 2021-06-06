f36324b8fd38923655e5e0b39a0051efc7ba68a7
Dominick Joo <dominickijoo@gmail.com>
2021-06-06 10:53:25 -0400

    something with main.py

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Congratulations, it's a web app!"
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
