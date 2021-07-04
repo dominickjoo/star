4de2c7d9a25492e319940ff0bf443214a09f7a73
Dominick Joo <dominickijoo@gmail.com>
2021-07-04 13:33:53 -0400

    commit something inmain.py?

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Congratulations, it's a web app!"
+    return "Grid-inputting UI goes here."
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
