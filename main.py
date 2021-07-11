9d8874cab1c22ff956482bd74c8a6b9a53f654e8
Dominick Joo <dominickijoo@gmail.com>
2021-07-11 18:10:48 -0400

    lots of things, oops

--- main.py
+++ main.py
@@ -1,19 +1,106 @@
 import tkinter as tk
 
-def calculate()
+# Size of square in grid
+GRID_SQUARE_SIZE = 40
+# Size of square in palette
+PALETTE_SQUARE_SIZE = 30
+# Size of space between squares in palette
+SPACE_SIZE = 15
+# Yellow, Orange, Red, Magenta, Purple, Blue, Cyan, Teal, Green, Lime
+# https://colorswall.com/palette/73/
+COLORS = ['#fff100', '#ff8c00', '#e81123', '#ec008c', '#68217a', '#00188f', '#00bcf2', '#00b294', '#009e49', '#bad80a']
 
-root = tk.Tk()
-root.title("Star Battle Solver")
+class SolverGUI(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self.title("Star Battle Solver")
+        self.frame2 = None
+        self.frame1 = tk.Frame(self)
+        self.frame1.pack()
 
-frame = tk.Frame(root)
-frame.pack()
+        num_regions_label = tk.Label(self.frame1, text="Number of rows/columns/regions (10 max):", padx=3, pady=3)
+        num_regions_label.pack()
 
-label = tk.Label(frame, text="Number of rows/columns:", padx=3, pady=3)
-label.pack()
+        num_regions_entry = tk.Entry(self.frame1, width=2)
+        num_regions_entry.pack()
 
-entry = tk.Entry(frame, width=5)
-entry.pack()
+        generate_grid_button = tk.Button(self.frame1, text='Generate grid',command=lambda: self.draw_frame2(
+                int(num_regions_entry.get()),
+            ))
+        generate_grid_button.pack()        
 
-root.bind("<Return>", calculate)
+    def draw_frame2(self, num_regions):
+        if self.frame2:
+            self.frame2.destroy()
 
-root.mainloop()
+        self.num_regions = num_regions
+
+        print(num_regions)
+
+        self.frame2 = tk.Frame(self)
+        self.frame2.pack()
+
+
+        color_palette = tk.Canvas(self.frame2,
+                    height=PALETTE_SQUARE_SIZE + 15,
+                    width=PALETTE_SQUARE_SIZE*num_regions + SPACE_SIZE*(num_regions-1) + 30)
+        color_palette.pack()
+
+        for color_num in range(num_regions):
+            color_palette.create_rectangle(
+                color_num*(PALETTE_SQUARE_SIZE + SPACE_SIZE) + 15,
+                15,
+                color_num*(PALETTE_SQUARE_SIZE + SPACE_SIZE) + PALETTE_SQUARE_SIZE + 15,
+                PALETTE_SQUARE_SIZE + 15,
+                                        fill=COLORS[color_num])
+
+        color_palette.bind("<Button-1>", self.get_color)
+
+
+        grid = tk.Canvas(self.frame2,
+                    height=GRID_SQUARE_SIZE*num_regions + 50,
+                    width=GRID_SQUARE_SIZE*num_regions + 50)
+        grid.pack()
+
+        for row in range(num_regions):
+            for col in range(num_regions):
+                grid.create_rectangle(GRID_SQUARE_SIZE*col + 25, 
+                                        GRID_SQUARE_SIZE*row + 25,
+                                        GRID_SQUARE_SIZE*(col+1) + 25,
+                                        GRID_SQUARE_SIZE*(row+1) + 25,
+                                        fill='')
+
+        grid.bind("<Button-1>", self.paint_square)
+
+        num_stars_label = tk.Label(self.frame1, text="Number of stars per row/column/region:", padx=3, pady=3)
+        num_stars_label.pack()
+
+        num_stars_entry = tk.Entry(self.frame1, width=2)
+        num_stars_entry.pack()
+
+        solve_button = tk.Button(self.frame1, text='Solve!',command=None)
+        solve_button.pack() 
+        
+        self.frame2.tkraise()
+
+    def get_color(self, event):
+        if event.y < 15 or (event.x % 45) < 15:
+            return
+
+        self.color = COLORS[event.x // 45]
+
+        print(event.x, event.y)
+        print(self.color)
+
+    def paint_square(self, event):
+        if not (25 < event.x < 425 and 25 < event.y < 425):
+            return
+
+
+
+
+
+        print(event.x, event.y)
+
+solver = SolverGUI()
+solver.mainloop()
